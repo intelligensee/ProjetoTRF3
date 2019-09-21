50d4b9308f84b930c3732060e2f8c96717340af7
ronal <ronal@DESKTOP-JNCCI2D>
2019-09-21 15:06:37 -0300

    ConnectionFactory: configurações para Azure

--- util/ConnectionFactory.php
+++ util/ConnectionFactory.php
@@ -2,30 +2,29 @@
 
 abstract class ConnectionFactory {
 
-    /*public static function getMySQLConnection(): PDO {
-        $sql = "mysql:host=localhost;dbname=vestibulinho_etec;charset=utf8";
-        $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-        $conn = new PDO($sql, 'root', '', $dsn_Options);
-        return $conn;
-    }*/
+    private static $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
-    public static function getMySQLConnectionVEtec(): PDO {
-        $sql = "mysql:host=localhost;dbname=id9351912_vestibulinho_etec;charset=utf8";
-        $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-        $conn = new PDO($sql, 'id9351912_rsp', '16rsp71VE', $dsn_Options);
-        return $conn;
-    }
-    
     public static function getMySQLConnection(): PDO {
-        $sql = "mysql:host=localhost;dbname=id10809310_intelligensee_trf3;charset=utf8";
-        $dsn_Options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
-        $conn = new PDO($sql, 'id10809310_cronotrf3', 'itlstrf3', $dsn_Options);
+        //return self::TRF3Local();
+        return self::TRF3Azure();
+    }
+
+    private static function TRF3Local(): PDO {
+        $sql = 'mysql:host=localhost;dbname=id10809310_intelligensee_trf3;charset=utf8';
+        $user = 'id10809310_cronotrf3';
+        $password = 'itlstrf3';
+        $conn = new PDO($sql, $user, $password, self::$dsn_Options);
         return $conn;
     }
-    
-    //dbname=id10809310_intelligensee_trf3
-    //user=id10809310_cronotrf3
-    //senha=itlstrf3
-    
-    //senha do site=trf3itls
+
+    private static function TRF3Azure(): PDO {
+        $sql = 'mysql:host=trf3crono.mariadb.database.azure.com;port=3306;dbname=trf3';
+        $user = "itlsadm@trf3crono";
+        $password = "Int3llig3nS33";
+        $options = array(PDO::MYSQL_ATTR_SSL_CA => 'C:/BaltimoreCyberTrustRoot.crt.pem');
+        return new PDO($sql, $user, $password, $options);
+    }
+
+    //senha do site WH: trf3itls
+    //azure senha: 40289653819
 }
